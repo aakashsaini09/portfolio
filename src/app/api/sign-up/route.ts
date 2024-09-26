0fe4f3c1291da4ac972551b07fe26717d222ee4f
aakashsaini09 <aakashsaini948585@gmail.com>
2024-09-26 13:53:19 +0530

    saveUser

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -17,11 +17,27 @@ export async function POST(request: Request) {
                 }, {status: 400})
             }
             const existingUserByEmail = await UserModel.findOne({email})
+            const verifyCode = Math.floor(100000 + Math.random() * 900000).toString()
             if(existingUserByEmail){
                 return Response.json({
                     success: false,
-                    message: "Username is "
+                    message: "Email is already taken"
                 })
+            }else{
+                let hashPassword = await bcrypt.hash(password, 10);
+                const expiryDate = new Date()
+                expiryDate.setHours(expiryDate.getHours() + 1);
+                const newUser = new UserModel({
+                    username,
+                    email,
+                    password: hashPassword,
+                    verifyCode,
+                    isVerified: false,
+                    verifyExpiry: expiryDate,
+                    isAcceptingMessage: true,
+                    message: []
+                })
+                await newUser.save()
             }
             
         } catch (err) {
