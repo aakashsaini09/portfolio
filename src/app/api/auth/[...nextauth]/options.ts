12fe5b853c4d399539f245d710c07348b44c5956
aakashsaini09 <aakashsaini948585@gmail.com>
2024-10-23 13:10:26 +0530

    crud

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,1 +1,24 @@
 // cred options or providers here.
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import  bcrypt from 'bcryptjs'
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/models/User";
+import { promises } from "dns";
+
+
+export const authOptions: NextAuthOptions = {
+    providers: [
+        CredentialsProvider({
+            id: "credentials",
+            name: "credentials",
+            credentials: {
+                email: { label: "Email", type: "text" },
+                password: { label: "Password", type: "password"}
+            },
+            async authorize(credentials: any):Promise<any>{
+
+            }
+        })
+    ]
+}
