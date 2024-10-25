2ee686c8bbe9f94029610cefadb609bd35c57457
aakashsaini09 <aakashsaini948585@gmail.com>
2024-10-25 16:25:47 +0530

    temp auth pages

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      page working
+    </div>
+  )
+}
+
+export default page
--- src/middleware.ts
+++ src/middleware.ts
@@ -5,17 +5,11 @@ export {default} from 'next-auth/middleware'
 export async function middleware(request: NextRequest) {
   const token = await getToken({ req: request})
   const url = request.nextUrl
-  if(token && 
-    (
-      url.pathname.startsWith('/sign-in') ||
-      url.pathname.startsWith('/sign-up') ||
-      url.pathname.startsWith('/verify') ||
-      url.pathname.startsWith('/') 
-    )
-){
-    return NextResponse.redirect(new URL('/dashboard', request.url))
-  }
-  return NextResponse.redirect(new URL('/home', request.url))
+      if(token && ( url.pathname.startsWith('/sign-in') || url.pathname.startsWith('/sign-up') || url.pathname.startsWith('/verify') || url.pathname.startsWith('/')))
+      {
+        return NextResponse.redirect(new URL('/dashboard', request.url))
+      }
+  // return NextResponse.redirect(new URL('/home', request.url))
 }
 
 
