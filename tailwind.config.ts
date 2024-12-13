30925abd6f07adcd1e6e6edf6fc08c2e240a773a
aakashsaini09 <aakashsaini948585@gmail.com>
2024-12-13 17:43:46 +0530

    styling setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+import { access } from "fs";
 import type { Config } from "tailwindcss";
 
 export default {
@@ -8,10 +9,28 @@ export default {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+	container: {
+		center: true,
+		padding: '15px',
+	},
+	screens: {
+		sm: '640px',
+		md: '768px',
+		lg: '960px',
+		xl: '1200px',
+	},
+	fontFamily: {
+		primary: "var(--font-jetbrainMono)"
+	},
   	extend: {
   		colors: {
   			background: 'var(--background)',
-  			foreground: 'var(--foreground)'
+  			foreground: 'var(--foreground)',
+			primary: '#1c1c22',
+			accent: {
+				DEFAULT: '#00ff99',
+				hover: '#00e187'
+			}
   		},
   		borderRadius: {
   			lg: 'var(--radius)',
