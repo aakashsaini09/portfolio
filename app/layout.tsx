dfb168e713dc0a34dfe8db15d8e3746f42beca25
aakashsaini09 <aakashsaini948585@gmail.com>
2024-12-13 18:04:15 +0530

    nav

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
 
 const jetBrainMono = JetBrains_Mono({
   subsets: ["latin"],
@@ -21,6 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${jetBrainMono.variable} antialiased`} >
+        <Header/>
         {children}
       </body>
     </html>
---
+++ components/Header.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link"
+import Nav from "./Nav"
+import { Button } from "./ui/button"
+
+const Header = () => {
+  return (
+    <header className="py-8 xl:py-12 text-white ">
+      <div className="container mx-auto flex justify-center items-center">
+        {/* logo here */}
+        <Link href={'/'}>
+            <h1 className="text-4xl font-semibold">
+                Aakash <span className="text-accent">.</span>
+            </h1>
+        </Link>
+        {/* desktop */}
+        <div className="hidden xl:flex items-center gap-8">
+            <Nav/>
+            <Link href={'/contact'}>
+                <Button>Hire me</Button>
+            </Link>
+        </div>
+        {/* mobile */}
+        <div className="xl:hidden">mobile</div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
