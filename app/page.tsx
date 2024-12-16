7e7acea04befea8013f8f9b08b0c5587e4864986
aakashsaini09 <aakashsaini948585@gmail.com>
2024-12-16 11:27:59 +0530

    stats

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ const Home = () => {
   return <section className="h-full">
       <div className="container mx-auto h-full">
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
-          <div className="text text-center xl:text-left ">
+          <div className="text text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Software Developer</span>
             <h1 className="h1 mb-6">Hello I'm <br /> <span className="text-accent">Aakash Saini</span></h1>
             <p className="max-w-[500px] mb-9 text-white/80">I excel at crafting elegant digital experiences and I am proficient in various programming languages and technologies.</p>
@@ -23,7 +23,7 @@ const Home = () => {
               </div>
             </div>
           </div>
-          <div className="photo">
+          <div className="photo order-1 xl:order-none mb-8 xl:mb-0">
             <Photo/>
           </div>
         </div>
