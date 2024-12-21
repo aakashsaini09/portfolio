b9e056c929c35e8c940345767e59c8753d1bdc08
aakashsaini09 <aakashsaini948585@gmail.com>
2024-12-21 14:19:32 +0530

    slider btns fixed

---
+++ components/WorkSliderBtns.tsx
@@ -0,0 +1,13 @@
+"use client";
+import { useSwiper } from "swiper/react";
+import { PiCaretLeftBold, PiCaretRightBold } from 'react-icons/pi'
+
+const WorkSliderBtns = ({containerStyles, btnStyles, iconsStyles}: any) => {
+    const swiper = useSwiper()
+  return  <div className={containerStyles}>
+      <button onClick={() => swiper.slidePrev()} className={btnStyles}><PiCaretLeftBold className={iconsStyles}/></button>
+      <button onClick={() => swiper.slideNext()} className={btnStyles}><PiCaretRightBold className={iconsStyles}/></button>
+    </div>
+}
+
+export default WorkSliderBtns
